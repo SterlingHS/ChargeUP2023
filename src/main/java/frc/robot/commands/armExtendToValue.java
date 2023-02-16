1440936c8c6f40a56a8821ff91ca75122a6e3a2d
Alexis Rivera <arivera@sps5.org>
2023-02-16 16:00:58 -0600

    Add command armExtendToValue with PIDController

---
+++ src/main/java/frc/robot/commands/armExtendToValue.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.ArmSystem;
+
+public class armExtendToValue extends CommandBase {
+  private final ArmSystem m_armsysyetem;
+  private PIDController m_pidController;
+  /** Creates a new armExtendToValue. */
+  public armExtendToValue(ArmSystem sub1, double destination) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_armsysyetem = sub1;
+    addRequirements(m_armsysyetem);
+    m_pidController = new PIDController(.5, .04, .04);
+    m_pidController.setSetpoint(destination);
+    m_pidController.setTolerance(Constants.kTurnToleranceDeg, Constants.kTurnRateToleranceDegPerS);
+    m_pidController.enableContinuousInput(0, 12000);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double output = m_pidController.calculate(m_armsysyetem.getPosition());
+    m_armsysyetem.extendArm(output);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_armsysyetem.stopArmMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return m_pidController.atSetpoint();
+  }
+}
